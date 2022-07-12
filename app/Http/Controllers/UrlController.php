74728aef3769afb8f81beb12ac3ed2cc7cae3ced
Vladislav <55162837+northernchar@users.noreply.github.com>
2022-07-12 21:34:07 +0300

    fix

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -182,7 +182,7 @@ class UrlController extends Controller
         }
         if ($document->has('h1')) {
             // $h1children = $document->first('h1')?->children();
-            $h1doc = $document?->first('h1');
+            $h1doc = $document->first('h1');
             $h1children = $h1doc ? $h1doc->children() : [];
             $h1text = array_map(fn($attr) => $attr->text(), $h1children);
             $h1 = implode('', $h1text);
